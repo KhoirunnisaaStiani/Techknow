d087b1ff65d5be50aff47531f15844f0bace2967
KhoirunnisaaStiani <khoirunnisaa.stiani@gmail.com>
2024-10-14 22:58:01 +0700

    Ini adalah pesan commit pertama anda

---
+++ android/app/src/main/java/com/example/techknoww/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.techknoww;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'beranda.dart';
+
+void main() {
+  runApp(ModulAjarApp());
+}
+
+class ModulAjarApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Modul Ajar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Beranda(),
+    );
+  }
+}
